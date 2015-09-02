f4527bb6a9eee862de69746a5222a2b884cf0c3a
William <william.marstrand@gmail.com>
2015-09-02 11:09:47 +0200

    Estimator

---
+++ DatagramEstimator.java
@@ -0,0 +1,26 @@
+public class DatagramEstimator{
+
+	public DatagramEstimator(){}
+
+	public static void estimateData(int datagramSize, int numbOfSentDatagrams, double transmissionTimeInterval){
+
+	}
+
+	//Helper methods
+	private int absoluteDataNumberLost(){
+
+	}
+
+	private double percentageDataLost(){
+
+	}
+
+	private int absoluteDataNumberDuplicated(){
+
+	}
+
+	private double percentageDataDuplicated(){
+
+	}
+
+}
