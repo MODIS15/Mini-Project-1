070b632abbf729784ae928cb78e3bbcec201fb8b
TVANO <thorolesen14@gmail.com>
2015-09-03 08:27:00 +0200

    Indent

--- UDPServer.java
+++ UDPServer.java
@@ -1,13 +1,17 @@
 import java.net.*;
 import java.io.*;
-public class UDPServer{
-    public static void main(String args[]){ 
+public class UDPServer
+{
+    public static void main(String args[])
+	{
     	DatagramSocket aSocket = null;
-		try{
+		try
+		{
 	    	aSocket = new DatagramSocket(7007);
 					// create socket at agreed port
 			byte[] buffer = new byte[1000];
- 			while(true){
+ 			while(true)
+			{
  				DatagramPacket request = new DatagramPacket(buffer, buffer.length);
   				aSocket.receive(request);     
     			DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(), 
@@ -17,8 +21,9 @@ public class UDPServer{
                 System.out.println("Packet:" + reply);	
     			aSocket.send(reply);
     		}
-		}catch (SocketException e){System.out.println("Socket: " + e.getMessage());
-		}catch (IOException e) {System.out.println("IO: " + e.getMessage());
-		}finally {if(aSocket != null) aSocket.close();}
+		}
+		catch (SocketException e){System.out.println("Socket: " + e.getMessage()); }
+		catch (IOException e) {System.out.println("IO: " + e.getMessage()); }
+		finally {if(aSocket != null) aSocket.close();}
     }
 }
