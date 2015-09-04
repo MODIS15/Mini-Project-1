c271b0c6654074833cfc86906f10d9a9e9e35dfe
William <william.marstrand@gmail.com>
2015-09-04 19:41:25 +0200

    Test Root

---
+++ Test/UDPServerTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by William on 9/4/2015.
+ */
+public class UDPServerTest {
+
+    @org.junit.Test
+    public void testMain() throws Exception {
+
+    }
+}
