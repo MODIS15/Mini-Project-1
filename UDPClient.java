5c267b1083459c4cd79b2037e6c9fe68b9050562
Dennis <dttn@itu.dk>
2015-09-04 14:25:00 +0200

    Almost done

--- UDPClient.java
+++ UDPClient.java
@@ -1,5 +1,3 @@
-import com.sun.deploy.util.StringUtils;
-
 import java.io.IOException;
 import java.net.*;
 import java.util.Scanner;
@@ -19,13 +17,26 @@ public class UDPClient
 	}
 
 	private void initialize(){
-		packet = setPacket();
-
-		if (packet != null) send(packet);
-		else {
-			System.out.println("Something went wrong...");
+		try
+		{
+			packet = setPacket();
+			if (packet != null)
+			{
+				send(packet);
+			}
+			else
+			{
+				System.out.println("Something went wrong...");
+				initialize();
+			}
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
 			initialize();
 		}
+
+
 	}
 
 	/**
@@ -39,13 +50,16 @@ public class UDPClient
 				"\n To use default values just press enter without any input. ");
 
 		Scanner scanner = new Scanner(System.in);
-		if (scanner.hasNext()) {
+		if (scanner.hasNext())
+		{
 			String input = scanner.nextLine();
 			String[] packetData = input.split("\\|");
 
 
-			if (isInputValid(packetData)) {
-				for (int i = 2; i < packetData.length; i++) {
+			if (isUserInputValid(packetData))
+			{
+				for (int i = 2; i < packetData.length; i++)
+				{
 					String tempMessage = packetData[i];
 					String messageWithHashCode = new String(tempMessage + "¤" + tempMessage.hashCode());
 					packetData[i] = messageWithHashCode;
@@ -57,7 +71,12 @@ public class UDPClient
 		else return null;
 	}
 
-	private boolean isInputValid(String[] packetData){
+	/**
+	 * Check if address and port is valid
+	 * @param packetData
+	 * @return a boolean regarding port and address is valid
+	 */
+	private boolean isUserInputValid(String[] packetData){
 		String address = packetData[0];
 		String port = packetData[1];
 
@@ -68,30 +87,64 @@ public class UDPClient
 	}
 
 
+	/**
+	 *
+	 * @param inputData
+	 */
 	private void send(String[] inputData){
 		DatagramSocket aSocket = null;
 		String address = inputData[0];
 		int port = Integer.getInteger(inputData[1]);
+		int tryCount = 3;
 
-		for(int i = 2; i < inputData.length ; i++){
-			System.out.println("Sending..." );
+		for(int i = 2; i < inputData.length ; i++) {
+			System.out.println("Sending message "+ i+"..." );
 
 			String message = inputData[i];
-			boolean isConnectionRecieve = false;
-
-			try {
+			try
+			{
 				aSocket = new DatagramSocket();
 				byte[] m = message.getBytes();
 				InetAddress aHost = InetAddress.getByName(address);
 				int serverPort = port;
-				DatagramPacket request =
-						new DatagramPacket(m, message.length(), aHost, serverPort);
+				DatagramPacket request = new DatagramPacket(m, message.length(), aHost, serverPort);
+
 				aSocket.send(request);
 
+
 				//Check if server has received datagram
-				checkReceive(aSocket);
+				System.out.println("Waiting for host...");
+				byte[] buffer = new byte[1000];
+				DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+				boolean packetOK = false;
 
+				//Checking until timeout
+				long endTimeMillis = System.currentTimeMillis() + 5000; //Timeout after 5 seconds
+				while (System.currentTimeMillis() < endTimeMillis){
+					if(checkReceive(aSocket,reply))
+					{
+						packetOK = true;
+						break;
+					}
+				}
 
+				//Resending or skipping message after a number of tries
+				if(!packetOK && tryCount > 0)
+				{
+					System.out.println("Timeout... Trying again.");
+					i--;
+					tryCount--;
+				}
+
+				else if (!packetOK && tryCount >= 0)
+				{
+					if(inputData.length < 4)
+						System.out.println("Message could not be sendt.");
+					else
+						System.out.println("Message could not be sendt. Skipping to next message.");
+				}
+
+				else System.out.println("Success.");
 
 
 			} catch (SocketException e) {
@@ -102,17 +155,21 @@ public class UDPClient
 				if (aSocket != null) aSocket.close();
 			}
 
-		}
+			//Resetting client
+			initialize();
 
+		}
 	}
 
-	private boolean checkReceive(DatagramSocket aSocket) throws IOException {
-		byte[] buffer = new byte[1000];
-		DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
-		while(reply.getAddress() == null)
-		{
-			aSocket.receive(reply);
-		}
+	private boolean checkReceive(DatagramSocket aSocket, DatagramPacket reply) throws IOException {
+		aSocket.receive(reply);
+
+		if(reply.getAddress() == null) return false;
+
+		int validationNumber = Integer.getInteger(new String(reply.getData()));
+		if(validationNumber == 0) System.out.println("Sent message was corrupt.");
+		return validationNumber == 1;
+
 
 	}
 
