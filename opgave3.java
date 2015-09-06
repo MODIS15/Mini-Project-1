d727241439f541134ca94f6e41f09ac5e1948e92
JakeMullit <jmmo@itu.dk>
2015-09-06 16:12:03 +0200

    Thread handling. Single thread + senderthread

--- opgave3.java
+++ opgave3.java
@@ -1,66 +1,75 @@
 import java.io.IOException;
 import java.net.*;
-import java.util.HashMap;
+import java.util.HashSet;
 
-/*
-Your programs must accept as input (a) datagram size, (b) number of datagrams sent and (c) interval between transmissions.
-The program must output (1) absolute number and percentage of lost datagrams and (2) absolute number and percentage of duplicated datagrams.
- It is acceptable if your estimate cannot distinguish between losing a single reply, losing a single response, or losing both a reply and a response.
- */
-
-public class opgave3 {
+public class opgave3
+{
 
 
-    //Minimum String memory usage (bytes) = 8 * (int) ((((no chars) * 2) + 45) / 8)
-    //Simplified : Multiply the number of characters of the String by two; Then add 38;
-
-
-    HashMap<String,Integer> occurrence = new HashMap<>();
-    DatagramSocket sendingSocket;
-
-    ReceiverThread receiver;
-    SendingThread sender;
-    boolean threadActive;
-
+    HashSet<String> occurrence = new HashSet<>(); //Received messages. Used to keep count on duplicate messages
+    DatagramSocket sendingSocket; //Sending socket
+    SendingThread sender;         //Sending thread. Splitting receiver and sender in two different threads
+    String fillerData;            //Filler data used to append package data to adjust package size
+    boolean SendingThreadIsStillActive = true;
+    String[] messageList; //Stores all sent messages.
+    int amountOfMessageReceived;
 
     public static void main(String[] args)
     {
-        opgave3 program = new opgave3(21,5,1);
+        System.out.println("Opgave 3 start");
+        System.out.println("Transmitting packages");
+        opgave3 program = new opgave3(50,6,1);
+
+        System.out.println("Amount of datagrams lost: " + program.amountOfLostDatagrams());
+        System.out.println("Amount of datagrams lost in percentage: " + program.amountOfLostDatagramsInPercentage());
+        System.out.println("Amount of datagrams duplicates: " + program.amountOFDuplicateDatagram());
+        System.out.println("Amount of datagrams duplicates in percentage: " + program.amountOFDuplicateDatagramInPercentage());
+
+
     }
 
-    String fillerData;
+
 
 
 
     public opgave3(int datagramSize, int amountOfDatagramsSent, int transmissionInterval)
     {
-        threadActive = true; //receiever thread will be active until false
+
         fillerData = createFillerData(datagramSize);
 
         try
         {
-            sendingSocket = new DatagramSocket();
-            receiver = new ReceiverThread(occurrence);
+            sendingSocket = new SocketUDP();
             sender = new SendingThread(sendingSocket, transmissionInterval,amountOfDatagramsSent);
-
-
-            Thread listenerThread = new Thread(receiver);
             Thread senderThread   = new Thread(sender);
-
-            listenerThread.start();
             senderThread.start();
-            System.out.println("Freedom!!!! Calc time!");
+
+
+            //Receiving thread
+            DatagramSocket socket = new DatagramSocket(7007);
+            socket.setSoTimeout(5000);
+            byte[] buffer = new byte[1000];
+            while(SendingThreadIsStillActive)
+            {
+                DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+                socket.receive(reply);
+                String message = new String(reply.getData());
+                updateOccurrenceRatings(message);
+                //System.out.println(message);
+            }
         }
+        catch (SocketTimeoutException e ){}
         catch (SocketException e) {e.printStackTrace();}
+        catch (IOException e) {e.printStackTrace();}
+
     }
 
-    public void closeThreads()
+
+    private void updateOccurrenceRatings(String reply)
     {
-        threadActive = false;
-
+        amountOfMessageReceived++;
+        occurrence.add(reply);
     }
-
-
 
     private String createFillerData(int memorySize)
     {
@@ -84,71 +93,33 @@ public class opgave3 {
         return _filerData;
     }
 
-
-    public void amountOfLostDatagrams()
+///Not Quit done
+    public int amountOfLostDatagrams()
     {
-
+        return messageList.length-occurrence.size();
     }
 
-    public void amountOfLostDatagramsInPercentage(){
-
-        int amountOfPackagesLost = 0;
-
-    }
-
-    public void amountOFDuplicateDatagram(){}
-
-    public void amountOFDuplicateDatagramInPercentage(){}
-
-
-
-    public class ReceiverThread implements Runnable
+    public double amountOfLostDatagramsInPercentage()
     {
-        private HashMap<String,Integer> occurrence;
-
-
-        public ReceiverThread(HashMap<String,Integer> occurrence)
-        {
-            this.occurrence = occurrence;
-        }
-        @Override
-        public void run()
-        {
-            DatagramSocket socket = null;
-                try
-                {
-                    socket = new DatagramSocket(7007);
-                    // create socket at agreed port
-                    byte[] buffer = new byte[1000];
-                    while(threadActive)
-                    {
-                        DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
-                        socket.receive(reply);
-                        String message = new String(reply.getData());
-                        updateOccurrenceRatings(message);
-                        System.out.println(message);
-                    }
-                }
-                catch (SocketException e){System.out.println("Socket: " + e.getMessage());}
-                catch (IOException e) {System.out.println("IO: " + e.getMessage());}
-                finally {if(socket != null) socket.close();}
-        }
-
-        /** Store data in occurrence map.
-         */
-        private void updateOccurrenceRatings(String reply)
-        {
-            if(occurrence.containsKey(reply))
-                occurrence.put(reply,1+occurrence.get(reply)); //If exist, then increment
-            else{occurrence.put(reply,1);} //Else create new entry
-        }
-
-        public HashMap<String,Integer> getOccurrenceData()
-        {
-            return occurrence;
-        }
+        if(occurrence.size()!=0)
+            return (((double) messageList.length- (double)occurrence.size())/messageList.length)*100;
+        else
+            return 0;
     }
 
+    public int amountOFDuplicateDatagram()
+    {
+        return amountOfMessageReceived-occurrence.size();
+    }
+
+    public double amountOFDuplicateDatagramInPercentage()
+    {
+        if(occurrence.size()!= 0)
+            return (((double)amountOfMessageReceived-(double)occurrence.size())/occurrence.size())*100;
+        else
+            return 0;
+    }
+///Not Quit done
 
 
 
@@ -157,36 +128,24 @@ public class opgave3 {
 
 
 
+public class SendingThread implements Runnable{
 
-
-
-
-
-
-
-
-
-
-    public class SendingThread implements Runnable{
-
-
-        String[] messageList;
         DatagramSocket socket;
         String IPDestination = "localhost";
         int _timeOut;
 
 
-        public SendingThread(DatagramSocket socket, int timeOut, int messagesToBeSent)
+        public SendingThread(DatagramSocket socket, int timeOut, int amountOfMessagesTOBeSent)
         {
             this.socket = socket;
-            messageList  = new String[messagesToBeSent];
+            messageList  = new String[amountOfMessagesTOBeSent];
             _timeOut = timeOut;
 
-
+            //Create filler data used to append package data to adjust package size
             for(int i = 0; i<messageList.length; i++)
             {
                 if(i>9)
-                    messageList[i]=i+fillerData;  //Make sure there is two ciffers string. Important for data size
+                    messageList[i]=i+fillerData;
                 else
                     messageList[i] ="0"+i+fillerData;
             }
@@ -211,7 +170,8 @@ public class opgave3 {
 
                 int port = 7007;
 
-                try {
+                try
+                {
                     InetAddress host = InetAddress.getByName(IPDestination);
                     DatagramPacket packet = new DatagramPacket(message, _message.length(), host, port);
                     socket.send(packet);
@@ -226,6 +186,9 @@ public class opgave3 {
                 try {Thread.sleep(_timeOut);}
                 catch (InterruptedException e) {e.printStackTrace();}
             }
+
+            //All messages has been sent. Signal listener to close
+            SendingThreadIsStillActive = false;
 
         }
     }
